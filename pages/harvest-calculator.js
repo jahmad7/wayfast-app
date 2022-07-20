81e80971ef772a706a08558052bac6d1537bf8be
Ahmed Amawi <ahmed@atgpharma.com>
2022-07-19 20:17:43 -0400

    updated views

--- pages/harvest-calculator.js
+++ pages/harvest-calculator.js
@@ -75,17 +75,37 @@ function Calculator() {
           <div className="lg:grid flex flex-col grid-cols-3  my-4 lg:mt-16  gap-y-8 lg:gap-x-12 lg:gap-y-2 ">
             <div />
             <div className="hidden lg:block col-span-2">
-              <h3 className="text-center text-2xl font-bold ">
+              <h3 className="text-center text-2xl font-bold flex flex-row  gap-2">
                 <span className="text-wayfastGreen font-extrabold">
                   $
                   {(
-                    ((harvest * plants * 90) / 3600) * 70 -
+                    ((harvest * plants * 60) / 3600) * 70 -
                     ((harvest * plants * 3) / 3600) * 70
                   )
                     .toFixed(2)
                     .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}{" "}
                 </span>{" "}
-                Monthly Savings
+                Monthly Savings{" "}
+                <div className="relative z-40 flex flex-col justify-center items-center group">
+                  <svg
+                    className="w-5 h-5"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                  <div className="absolute top-10 w-48 z-50 flex flex-col items-center hidden  mb-6 group-hover:flex">
+                    <div className="w-3 h-3 -mt-3 transform rotate-45 bg-gray-600" />
+                    <span className="relative lowercase -mt-2  p-2 text-xs leading-none text-white whitespace-no-wrap bg-gray-600 shadow-lg rounded-md">
+                      Based on $35/hr rate
+                    </span>
+                  </div>
+                </div>
               </h3>
               <p className="text-right text-gray-900 text-sm">monthly costs</p>
             </div>
@@ -98,24 +118,22 @@ function Calculator() {
                 >
                   # of plants per harvest [avg]
                 </label>
-                <p className="flex flex-row justify-end text-xs">
-                  {plants.toFixed(0).replace(/\B(?=(\d{3})+(?!\d))/g, ",")}{" "}
-                  Plants
-                </p>
                 <input
-                  id="default-range"
-                  type="range"
+                  type="number"
                   min={100}
                   max={5000}
                   defaultValue={1000}
                   step={50}
-                  onChange={(e) => setPlants(parseInt(e.target.value))}
-                  className="w-full h-2 "
+                  onChange={(e) => {
+                    let value = parseInt(e.target.value);
+                    if (value < 1) {
+                      e.target.value = 1;
+                      value = 1;
+                    }
+                    setPlants(value);
+                  }}
+                  className="shadow-sm p-2 block w-full sm:text-sm border border-wayfastGreen focus:outline-none focus:border-atgBlue rounded-xl"
                 />
-                <p className="flex flex-row justify-between text-sm text-gray-700">
-                  <span>100</span>
-                  <span>5,000</span>
-                </p>
               </div>
               <div>
                 <label
@@ -145,17 +163,37 @@ function Calculator() {
               </div>
             </div>
             <div className="block lg:hidden col-span-2">
-              <h3 className="text-center text-2xl font-bold ">
-                <span className="text-wayfastGreen">
+              <h3 className="text-center text-2xl font-bold flex flex-row  gap-2">
+                <span className="text-wayfastGreen font-extrabold">
                   $
                   {(
-                    ((harvest * plants * 90) / 3600) * 70 -
+                    ((harvest * plants * 60) / 3600) * 70 -
                     ((harvest * plants * 3) / 3600) * 70
                   )
                     .toFixed(2)
                     .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}{" "}
                 </span>{" "}
-                Monthly Savings
+                Monthly Savings{" "}
+                <div className="relative z-40 flex flex-col justify-center items-center group">
+                  <svg
+                    className="w-5 h-5"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                  <div className="absolute top-10 -right-8 w-48 z-50 flex flex-col items-center hidden  mb-6 group-hover:flex">
+                    <div className="w-3 h-3 -mt-3  ml-24 transform rotate-45 bg-gray-600" />
+                    <span className="relative lowercase -mt-2  p-2 text-xs leading-none text-white whitespace-no-wrap bg-gray-600 shadow-lg rounded-md">
+                      Based on $35/hr rate
+                    </span>
+                  </div>
+                </div>
               </h3>
             </div>
             <div className="col-span-2 grid grid-cols-5  lg:pl-4  gap-x-4 lg:gap-x-12 lg:border-l border-gray-100 w-full">
@@ -163,12 +201,12 @@ function Calculator() {
                 <p>Manual</p>
               </div>
               <div className="flex flex-col justify-center items-center font-medium col-span-2">
-                <p className="text-red-700">90 seconds/plant</p>
+                <p className="text-red-700">60 seconds/plant</p>
               </div>
               <div className="flex flex-col justify-center font-semibold text-red-700 items-end col-span-2">
                 <p>
                   $
-                  {(((harvest * plants * 90) / 3600) * 70)
+                  {(((harvest * plants * 60) / 3600) * 70)
                     .toFixed(2)
                     .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
                 </p>
